296d1014f66c43938cab955d100f9cc9ed2f8f66
Yigit Demirko <46991541+yigitdemirko@users.noreply.github.com>
2026-04-28 16:37:49 +0300

    test(order-service): add unit tests for order service (#16)

--- services/order-service/src/test/java/com/marketplace/order/OrderServiceApplicationTests.java
+++ services/order-service/src/test/java/com/marketplace/order/OrderServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.marketplace.order;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class OrderServiceApplicationTests {
 
 	@Test
