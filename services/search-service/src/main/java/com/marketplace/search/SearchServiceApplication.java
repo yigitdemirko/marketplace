4a4b6257a33185a69358fc821386024c48e6661d
Yigit Demirko <ygtdemirko@gmail.com>
2026-04-28 10:19:34 +0300

    feat(search-service): add search service with elasticsearch integration

---
+++ services/search-service/src/main/java/com/marketplace/search/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
