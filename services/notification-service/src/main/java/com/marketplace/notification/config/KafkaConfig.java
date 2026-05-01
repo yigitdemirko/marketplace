ba75bf13546f1dd59226aa73c728c22d04c21b12
Yigit Demirko <ygtdemirko@gmail.com>
2026-05-01 13:24:09 +0300

    feat(kafka): add dead letter queue to all Kafka consumers

---
+++ services/notification-service/src/main/java/com/marketplace/notification/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.marketplace.notification.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public DefaultErrorHandler kafkaErrorHandler(KafkaTemplate<String, Object> kafkaTemplate) {
+        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+        return new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3L));
+    }
+}
